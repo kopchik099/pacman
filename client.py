ba937b237525299ad08643ef3c02ee2bd5c18413
kopchik099 <bsmega666@gmail.com>
2023-05-02 22:00:51 +1000

    client_server_start

--- client.py
+++ client.py
@@ -1,8 +1,22 @@
 import socket
 import time
-socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket2.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-socket2.connect(('localhost', 10000))
-while True:
-    socket2.send('говнядина'.encode())
+import pygame
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+sock.connect(('localhost', 10000))
+pygame.init()
+height = 500
+width = 500
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('пакман онлайн')
+run = True
+players = []
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    sock.send('лукас'.encode())
+    data = sock.recv(1024).decode()
+    print(f'получил: {data}')
     
+pygame.quit()
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('говядина')
+import socket
+import time
+main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+main_socket.bind(('localhost', 10000))
+main_socket.setblocking(False)
+main_socket.listen(5)
+print('сокет создан')
+players = []
+while True:
+    try: 
+        new_socket, addr = main_socket.accept()
+        print(f'подключился {addr}')
+        main_socket.setblocking(False)
+        players.append(new_socket)
+    except BlockingIOError:
+        pass
+    for i in players:
+        try:
+            data = i.recv(1024).decode()
+            print(f'получил: {data}')
+        except:
+            pass
+    for sock in players:
+        try:
+            sock.send('штукатурка'.encode())
+        except:
+            players.remove(sock)
+            sock.close()
+            print('сокет закрыт')
+    time.sleep(1)
+
