3e53f01bab93cd294030949f2cbe07946609cf1c
kopchik099 <bsmega666@gmail.com>
2023-06-06 17:57:09 +1000

    dz 3

--- codewars/solution.py
+++ codewars/solution.py
@@ -1,8 +1,9 @@
-def filter_list(l):
-    'return a new list with the strings filtered out'
-    sp = []
-    for i in l:
-        if type(i) != str:
-            sp.append(i)
-    return sp
-            
+def solution(string, ending):
+    # your code here...
+    string1 = len(string) - len(ending)
+    string2 = len(string) - string1
+    string3 = string[string1:]
+    if string3 == ending:
+        return True
+    else:
+        return False
