bb050a6531e3ec9a2b2afb77cd01fce4559be0b0
kopchik099 <bsmega666@gmail.com>
2023-06-07 19:50:29 +1000

    dz4

--- codewars/solution2.py
+++ codewars/solution2.py
@@ -1,8 +1,6 @@
-def open_or_senior(data):
-    res = []
-    for i in data:
-        if i[0] >= 55 and i[1] > 7:
-            res.append('Senior')
-        else:
-            res.append('Open')
-    return res
+def is_isogram(string):
+    string = string.lower()
+    for i in string:
+        if string.count(i) > 1: 
+            return False
+    return True
